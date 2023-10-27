a5e1bd7adb6e9f441b777d28b183eb67a3b36dd6
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-10-27 20:41:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+// JavaScript (script.js)
+
+let currentExpression = "";
+
+function addToDisplay(value) {
+  currentExpression += value;
+  document.getElementById("display").textContent = currentExpression;
+}
+
+function clearDisplay() {
+  currentExpression = "";
+  document.getElementById("display").textContent = "0";
+}
+
+function removeLastCharacter() {
+  currentExpression = currentExpression.slice(0, -1);
+  document.getElementById("display").textContent = currentExpression;
+}
+
+function calculateResult() {
+  try {
+    currentExpression = eval(currentExpression);
+    document.getElementById("display").textContent = currentExpression;
+  } catch (error) {
+    document.getElementById("display").textContent = "Error";
+  }
+}
